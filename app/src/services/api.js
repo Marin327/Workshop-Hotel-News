5d7230da58868d48d140bb3e62a0b7d0a587a47a
Marin Marinov <108364779+Marin327@users.noreply.github.com>
2024-04-19 21:06:18 +0300

    api.js

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-// Рута за създаване на нова резервация
+
 app.post('/api/reservations', (req, res) => {
     try {
         const newReservation = reservationService.createReservation(req.body);
@@ -17,13 +17,13 @@ app.post('/api/reservations', (req, res) => {
     }
 });
 
-// Рута за получаване на всички резервации
+
 app.get('/api/reservations', (req, res) => {
     const reservations = reservationService.getAllReservations();
     res.status(200).json(reservations);
 });
 
-// Рута за получаване на резервация по идентификационен номер
+
 app.get('/api/reservations/:id', (req, res) => {
     const reservation = reservationService.getReservationById(req.params.id);
     if (reservation) {
